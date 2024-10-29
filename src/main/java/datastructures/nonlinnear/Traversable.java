3c5a80e4287574c0a2660831c82a1185d18a9edc
ArctikCircle <arctikcircle.app@gmail.com>
2024-10-29 21:48:59 +0530

    Implemented BinaryTree in back-end

---
+++ src/main/java/datastructures/nonlinnear/Traversable.java
@@ -0,0 +1,27 @@
+package datastructures.nonlinnear;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public interface Traversable<T> extends Iterable<T> {
+    @NotNull
+    @Override
+    default Iterator<T> iterator() {
+        return iterator(Traversal.PREORDER);
+    }
+
+    @Override
+    default void forEach(Consumer<? super T> action) {
+        iterator().forEachRemaining(action);
+    }
+
+    Iterator<T> iterator(Traversal traversal);
+    enum Traversal {
+        INORDER,
+        PREORDER,
+        POSTORDER
+    }
+}
