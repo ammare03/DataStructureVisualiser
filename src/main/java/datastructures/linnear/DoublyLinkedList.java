ab6bb2dda89599f0d2c8daabee61b925465b777f
ArctikCircle <arctikcircle.app@gmail.com>
2024-11-08 15:28:04 +0530

    done some ui changes and fixed issues caused by previous commit

--- src/main/java/datastructures/linnear/DoublyLinkedList.java
+++ src/main/java/datastructures/linnear/DoublyLinkedList.java
@@ -116,6 +116,9 @@ public class DoublyLinkedList<T> extends LinkedList<T>{
             }
             iterator = iterator.next;
         }
+        if (iterator.next == null) {
+            throw new UnderflowException("Cannot remove the only node!");
+        }
         iterator.next = iterator.next.next;
         if (iterator.next != null) {
             iterator.next.previous = iterator;
